7346d1cbb151e5847b44107493187153ceb8d5ae
Mohcine <mohcineelarar@gmail.com>
2023-12-04 18:25:58 +0100

    Update

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# app.py
+# main.py
 import streamlit as st
 from home import show_home_page
 from signup import show_signup_page
 from login import show_login_page
 
+# Initialisez les clés clicked_1 et clicked_2 si elles ne sont pas présentes dans st.session_state
+if 'clicked_1' not in st.session_state:
+    st.session_state.clicked_1 = False
+if 'clicked_2' not in st.session_state:
+    st.session_state.clicked_2 = False
 
-
-# if 'sidebar_state' not in st.session_state:
-#     st.session_state.sidebar_state = 'collapsed'
-# st.set_page_config(initial_sidebar_state=st.session_state.sidebar_state)
-
+# Initialisez la clé 'page' si elle n'est pas présente dans st.session_state
 if 'page' not in st.session_state:
     st.session_state.page = 'home'
-
 
 # Page principale
 if st.session_state.page == 'home':
@@ -26,5 +26,3 @@ elif st.session_state.page == 'login':
 
     if st.sidebar.button("home"):
         st.session_state.page = 'home'
-
-
